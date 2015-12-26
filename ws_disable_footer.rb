19f45ea0e71dbad2bdcde4a17f30979b7afad340
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-26 13:52:30 +0900

    for mikutter 3.3.0

--- ws_disable_footer.rb
+++ ws_disable_footer.rb
@@ -4,9 +4,8 @@ module Gtk
       if postable?
         return unless before_post
         text = widget_post.buffer.text
-        text += UserConfig[:footer] if add_footer? && !(text =~ /^\s/)
+        text += UserConfig[:footer] if use_blind_footer? && !(text =~ /^\s/)
         @posting = service.post(:message => text){ |event, msg|
-          notice [event, msg].inspect
           case event
           when :start
             Delayer.new{ start_post }
